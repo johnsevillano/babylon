18d5b25fb9b69a60c5ae68536ea0fb13d20e307f
gschlereth <gschlereth@gmail.com>
2011-10-23 10:31:42 +0000

    Babel.Services.UnitTests project renamed to Babel.Services.Tests.

---
+++ services/dotnet/BabelServices/Babel.Services.Tests/SchemaGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+using Babel.Services.Domain;
+
+
+namespace Babel.Services.Tests
+{
+
+    [TestClass]
+    public class SchemaGenerator
+    {
+        [TestMethod]
+        public void GenerateSchema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Profile).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+        }
+
+    }
+
+}
