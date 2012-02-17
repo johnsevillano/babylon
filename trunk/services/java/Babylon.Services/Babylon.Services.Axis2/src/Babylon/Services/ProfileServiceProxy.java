bb0b145cd4eb9cb18f581f37273aa53266ce99e5
gschlereth <gschlereth@758bde35-ae44-bda5-5568-4671e89e9d45>
2012-02-17 13:07:14 +0000

    Babylon.Services.Axis2 - Creación del proyecto para los servicios web Axis2.

---
+++ trunk/services/java/Babylon.Services/Babylon.Services.Axis2/src/Babylon/Services/ProfileServiceProxy.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Babylon.Services;
+
+/**
+ *
+ * @author guille
+ */
+public class ProfileServiceProxy {
+
+    /**
+     * Sample method
+     */
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+    
+}
