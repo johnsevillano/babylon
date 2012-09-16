bfba15c71e8bc47f2c4b46b2dfd512692b2ee820
gschlereth <gschlereth@758bde35-ae44-bda5-5568-4671e89e9d45>
2012-09-16 20:15:35 +0000

    Babylon.Site - EF package updated to 5.0. Also added new BabylonMembershipProvider.

---
+++ trunk/front/Babylon/Babylon.Site/Providers/BabylonMembershipProvider.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace Babylon.Site.Providers
+{
+    public class BabylonMembershipProvider : System.Web.Security.MembershipProvider
+    {
+        private string _enablePasswordRetrieval;
+        private string _enablePasswordReset;
+        private string _requiresQuestionAndAnswer;
+        private string _requiresUniqueEmail;
+        private string _maxInvalidPasswordAttempts;
+        private string _minRequiredPasswordLength;
+        private string _minRequiredNonalphanumericCharacters;
+        private string _passwordAttemptWindow;
+        private string _applicationName;
+
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            _enablePasswordRetrieval = config["_enablePasswordRetrieval"];
+            _enablePasswordReset = config["_enablePasswordReset"];
+            _requiresQuestionAndAnswer = config["_requiresQuestionAndAnswer"];
+            _requiresUniqueEmail = config["_requiresUniqueEmail"];
+            _maxInvalidPasswordAttempts = config["_maxInvalidPasswordAttempts"];
+            _minRequiredPasswordLength = config["_minRequiredPasswordLength"];
+            _minRequiredNonalphanumericCharacters = config["_minRequiredNonalphanumericCharacters"];
+            _passwordAttemptWindow = config["_passwordAttemptWindow"];
+            _applicationName = config["_applicationName"];
+
+            base.Initialize(name, config);
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ApplicationName"];
+            }
+            set
+            {
+            }
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Web.Security.MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out System.Web.Security.MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Web.Security.MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Web.Security.MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Web.Security.MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Web.Security.MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Web.Security.MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Web.Security.MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(System.Web.Security.MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
