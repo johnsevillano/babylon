e88a6b3a3100c902288147733756b96fd20c95d3
gschlereth@gmail.com <gschlereth@gmail.com@758bde35-ae44-bda5-5568-4671e89e9d45>
2012-09-25 10:53:36 +0000

    Babylon.Site - Some refactorization.

--- trunk/front/Babylon/Babylon.Site/Controllers/HomeController.cs
+++ trunk/front/Babylon/Babylon.Site/Controllers/HomeController.cs
@@ -8,13 +8,15 @@ namespace Babylon.Site.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Babylon!";
 
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             return View();
